844a00f47efddb998c92df867872a6e5ed99e9d6
Ana <karajovanovic.ana@gmail.com>
2022-08-28 03:23:20 +0200

    commit urls

--- learnFrenchProject/learnFrenchProject/settings.py
+++ learnFrenchProject/learnFrenchProject/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'learn-french-project.herokuapp.com/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = 'learn-french-project.herokuapp.com/static/'
+STATIC_ROOT = '/static/'
 
 
 # Default primary key field type
